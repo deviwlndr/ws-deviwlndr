0b0da18f72d993d4c01292edd5506a2607253f2b
deviwlndrr <wdevi443@gmail.com>
2024-12-03 15:27:52 +0700

    push

--- config/cors.go
+++ config/cors.go
@@ -19,8 +19,7 @@ var origins = []string{
 	"https://deviwlndr.github.io",
 	"https://mhrndiva.github.io",
 	"http://127.0.0.1:5501",
-	"http://127.0.0.1:5500",
-	"http://127.0.0.1:8080",
+
 	"http://127.0.0.1:55859",
 
 

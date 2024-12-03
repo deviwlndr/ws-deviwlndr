affa718872ec61bd535ca2d7cf795deaecdbcdcd
deviwlndrr <wdevi443@gmail.com>
2024-12-03 15:28:04 +0700

    push

--- config/cors.go
+++ config/cors.go
@@ -17,10 +17,12 @@ var origins = []string{
 	"https://iteung.ulbi.ac.id",
 	"https://whatsauth.github.io",
 	"https://deviwlndr.github.io",
-	"https://mhrndiva.github.io",
-	"http://127.0.0.1:5501",
+    "http://127.0.0.1:5501",
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:8080",
+    "https://mhrndiva.github.io",
+    "http://127.0.0.1:55859",
 
-	"http://127.0.0.1:55859",
 
 
 }
